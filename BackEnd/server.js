fd584ae7a6a13666b4cb279876e486131bdf25e8
rachid aourik <rachidaourik18@gmail.com>
2026-03-11 01:05:58 +0000

    solve problems to run Backend

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -56,6 +56,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
